b7e0b1b1abab2f168660374b4b07d9674f1306ab
Unchained <aukachainx@gmail.com>
2020-04-24 06:40:09 +0700

    👅[UPDATE]

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -6,10 +6,12 @@ import StatusType from "./StatusType";
 import Pdpa from "./Pdpa";
 import Logo from "../core/Logo";
 import QRPayment from "./QRPayment";
+import Complete from "./Complete";
+import Upload from "./Upload";
 import { Stepper, Step, StepLabel, Button } from "@material-ui/core";
 
 function getSteps() {
-  return ["information", "Address", "Health", "Status", "Payment", "Upload"];
+  return ["information", "Address", "Health", "Status", "Payment", "Upload", "Finish"];
 }
 
 export default function Register() {
@@ -31,6 +33,7 @@ export default function Register() {
           <Logo />
         </div>
         <div className="col-12">
+
           <Stepper activeStep={activeStep} alternativeLabel>
             {steps.map((label) => (
               <Step key={label}>
@@ -38,6 +41,7 @@ export default function Register() {
               </Step>
             ))}
           </Stepper>
+          
           <div className="text-center mx-auto">
             <div className="p-3 w-full lg:w-1/3 overflow-hidden py-4 rounded shadow mx-auto">
               {activeStep === 0 && <Info1 />}
@@ -45,6 +49,8 @@ export default function Register() {
               {activeStep === 2 && <Info3 />}
               {activeStep === 3 && <StatusType />}
               { activeStep === 4 &&  <QRPayment />}
+              { activeStep === 5 &&  <Upload />}
+              { activeStep === 6 &&  <Complete />}
             </div>
           </div>
           <div className="col-12 col-sm-8 col-md-8 col-lg-4 mx-auto my-3">
@@ -65,7 +71,7 @@ export default function Register() {
                   onClick={handleNext}
                   // text={activeStep === steps.length - 1 ? "Submit" : "Next"}
                 >
-                  {activeStep === steps.length - 1 ? "Submit" : "Next"}
+                  {activeStep === steps.length - 1 ? "Done" : "Next"}
                 </Button>
               </div>
             </div>
