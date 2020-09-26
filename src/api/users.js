5385a614e3f1b963e186bd50fcfa255fbdb9ab67
sarinrat charoenkunasit <>
2020-09-26 15:27:44 +0700

    ✨[UPDATE] api

--- src/api/users.js
+++ src/api/users.js
@@ -4,3 +4,7 @@ export const apiFetchUsers = () => {
     return appAxios().get(`${process.env.REACT_APP_BACKEND}/users`)
 }
 
+export const apiFetchUsersID = (id) => {
+    return appAxios().get(`${process.env.REACT_APP_BACKEND}/users/${id}`)
+}
+
