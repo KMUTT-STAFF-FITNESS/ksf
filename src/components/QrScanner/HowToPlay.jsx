184924e516856d56ebe74bac3a6964a6b6ede08c
sarinrat charoenkunasit <>
2020-09-28 02:00:30 +0700

    ✨[UPDATE]how to play

--- src/components/QrScanner/HowToPlay.jsx
+++ src/components/QrScanner/HowToPlay.jsx
@@ -1,25 +1,52 @@
-import React, { Component } from 'react'
+import React, {  useState } from "react";
+import Logo from "../core/Logo";
+import BtnBack from "../core/BtnBack";
+import { navigate } from "@reach/router";
 
 
-class HowToPlay extends Component {
-  state = {
-    result: 'คำอธิบายที่ 1,2,3,4'
-  }
-  render() {
-    return (
-      <div>
-        <video controls>
-          <source src={"video_test/test.mp4"} type='video/mp4' />
-        </video>
-
-        <div class="text">
-                <p className="text-gray-700 text-lg font-bold">คำอธิบาย</p>
-         </div>
-        <p>{this.state.result}</p>
+export default function HowToPlay() {
+  const [howToPlay, setHowToPlay] = useState({
+    detail:"aaa"
+  })
+  return (
+    <div className="max-w-screen-xl mx-auto min-h-screen">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <Logo />
+            </div>
+            <div className="p-3 w-full lg:w-1/3 overflow-hidden py-4 rounded shadow mx-auto">
+              <video controls>
+                <source src={"video_test/test.mp4"} type="video/mp4" />
+              </video>
+              <div class="text-center mx-auto my-4">
+                <p className="text-gray-700 text-center text-lg ">
+                  {howToPlay.detail}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-6 col-lg-4 text-center mx-auto my-3">
+            <BtnBack
+              className="buttonBack"
+              onClick={() => navigate("/qrscanner")}
+              text="Back"
+            />
+          </div>
+        </div>
       </div>
-    )
-  }
-
+  )
 }
 
-export default HowToPlay;
+// class HowToPlay extends Component {
+//   state = {
+//     result: "คำอธิบายที่ 1,2,3,4",
+//   };
+//   render() {
+//     return (
+      
+//     );
+//   }
+// }
+
+// export default HowToPlay;
