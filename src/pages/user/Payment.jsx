8497b30a9f3c7be063254f5cd6a9f3db06994b6c
sarinrat charoenkunasit <>
2020-10-24 01:06:46 +0700

    ✨[ADD]payment page

---
+++ src/pages/user/Payment.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import QRpayment from '../../components/Register/QRPayment'
+import Logo from "../../components/core/Logo";
+
+export default function Payment() {
+    return (
+        <div className="container">
+        <div className="col-12">
+            <Logo/>
+          <div className=" mx-auto">
+            <div className=" p-3 w-full  overflow-hidden py-4 rounded shadow mx-auto">
+            <QRpayment/>
+        </div>
+        </div>
+        </div>
+        </div>
+    )
+}
