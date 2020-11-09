be926d66c82806c2586a8a0fb10502b71d3e9108
sarinrat charoenkunasit <>
2020-11-10 01:42:42 +0700

    ✨[UPDATE]PDPA

--- src/components/Register/Pdpa.jsx
+++ src/components/Register/Pdpa.jsx
@@ -5,22 +5,64 @@ import BtnBack from "../core/BtnBack";
 
 export default function Pdpa() {
   return (
-    <>
-      <p className="col-12 text-gray-700 text-lg py-2">
-        "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
-        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
-        Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse
-        quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo
-        voluptas nulla pariatur?"
-      </p>
-    </>
+    <div className="max-w-screen-xl mx-auto min-h-screen">
+      <div className=" p-3 w-full  overflow-hidden py-4 rounded shadow mx-auto">
+        <p className="text-gray-700 text-lg font-bold ">ข้อกำหนดและเงื่อนไข</p>
+        <p className=" text-gray-700 text-lg py-2">
+          “KSF” เป็นเว็ปแอปพลิเคชันของคณะเทคโนโลยีสารสนเทศ
+          ใช้สำหรับการเป็นสมาชิกฟิตเนสของมหาวิทยาลัยเทคโนโลยีพระจอมเกล้าธนบุรี
+          โดยผู้ใช้บริการตกลงยินยอมผูกพันตามข้อตกลงและเงื่อนไขการใช้บริการ KSF
+          ดังต่อไปนี้
+        </p>
+        <p className="text-gray-700 text-left">1. คำนิยาม </p>
+        <br></br>
+        <p className="text-gray-700 text-left">“ข้อมูลส่วนบุคคล” หมายถึง ข้อมูลหรือสิ่งใดๆ
+        ก็ตามที่เป็นเครื่องบ่งชี้ให้เห็นและเข้าใจถึงเรื่องราวหรือลักษณะเฉพาะตัวของบุคคลหนึ่งบุคคลใด
+        เช่น ประวัติการศึกษา ฐานะการเงิน ประวัติสุขภาพ ประวัติการทำงาน
+        ประวัติอาชญากรรม ฯลฯ
+        ซึ่งมีชื่อหรือสิ่งบอกลักษณะที่ทำให้รู้ตัวบุคคลผู้นั้นได้<br></br>
+        ”ข้อมูลการโอนเงินอิเล็กทรอนิกส์” หมายความถึง
+        ข้อมูลในการทำรายการสั่งโอนเงินและ/หรือเบิกใช้วงเงินสินเชื่อและ/หรือใช้บริการอื่นๆ
+        โดยผู้ขอใช้บริการผ่านเป๋าตัง ในการใช้บริการตามข้อตกลงนี้
+        ซึ่งผู้ขอใช้บริการเป็นผู้กำหนดข้อมูลรายละเอียดต่างๆ เช่น
+        ประเภทการโอนเงิน หมายเลขบัญชีเงินฝาก วันที่รายการมีผล
+        วันที่รายการหมดอายุ ผู้ส่งคำสั่งโอนเงิน (Releaser)
+        และ/หรือผู้ยกเลิกรายการรับเงิน (Unwinder)
+        จำนวนเงินที่ผู้ขอใช้บริการจะโอนให้แก่ผู้รับโอนเงิน
+        และ/หรือเลขที่บัญชีเงินฝากของผู้รับโอนเงิน
+        เลขที่ประจำตัวประชาชนของผู้รับโอนเงิน เป็นต้น</p>
+        <br></br><p className="text-gray-700 text-left">2. เงื่อนไขการสมัครใช้บริการ</p> <br></br>
+        <p className="text-gray-700 text-left">2.1 ผู้ขอใช้บริการสามารถสมัครใช้บริการ KSF
+        โดยใช้ข้อมูลส่วนบุคคลลงทะเบียนการเป็นสมาชิก
+        และจะต้องมีการชำระค่าบริการเป็นรายปี
+        หรือครั้งเดียวตลอดชีพตามประเภทสมาชิกที่เลือก <br></br>
+        2.2 ห้องฟิตเนสของมหาวิทยาลัยเทคโนโลยีพระจอมเกล้า ธนบุรี เปิดให้บริการ.......</p>
+        <br></br><p className="text-gray-700 text-left">3. การใช้และการเก็บรักษาข้อมูลส่วนตัว</p> <br></br>
+        <p className="text-gray-700 text-left"> 3.1 ในการใช้บริการทุกครั้ง ผู้ขอใช้บริการจะต้องใส่รหัสผู้ใช้งาน (User
+        ID/ Email) และรหัสผ่าน (Password) ในการเข้าสู่ระบบ
+        หากไม่ถูกต้องจะไม่สามารถเข้าสู่หน้าเว็ปได้ 
+        <br></br>3.2
+        ผู้ขอใช้บริการต้องรักษารหัสผู้ใช้งาน (User ID) รหัสผ่าน (Password)
+        ไว้เป็นอย่างดี และเก็บไว้ในที่ปลอดภัย มิให้สูญหาย
+        รวมทั้งเก็บรักษาไว้เป็นความลับเฉพาะตัวของผู้ขอใช้บริการเท่านั้นหากเกิดความเสียหายประการใดจากการสูญหายหรือการเปิดเผยรหัสต่างๆ
+        ดังกล่าว หรือจากเหตุอื่นใด KSF ไม่จำต้องรับผิดชอบใดๆ ทั้งสิ้น ทั้งนี้
+        <br></br>3.3 กรณีผู้ขอใช้บริการลืมรหัสผู้ใช้งาน (User ID)
+        สามารถติดต่อขอรับได้ด้วยตนเองที่ห้องฟิตเนสของมหาวิทยาลัยเทคโนโลยีพระจอมเกล้า
+        ธนบุรี ในเวลาทำการ</p>
+        <br></br><p className="text-gray-700 text-left">4. บัญชี </p><br></br>
+        <p className="text-gray-700 text-left">4.1 เมื่อผู้ใช้ให้ข้อมูลเกี่ยวกับตนเองแก่บริษัทฯ
+        ผู้ใช้จะต้องให้ข้อมูลที่เป็นจริง ถูกต้องและครบถ้วนในขณะที่ใช้บริการฯ
+        และปรับปรุงข้อมูลดังกล่าวให้เป็นปัจจุบันอยู่ตลอดเวลา</p>
+        <br></br><p className="text-gray-700 text-left">5. การคุ้มครองข้อมูลส่วนบุคคล</p> <br></br>
+        <p className="text-gray-700 text-left">5.1 KSF ให้ความสำคัญแก่ความเป็นส่วนตัวของผู้ใช้งาน เป็นลำดับแรก
+        <br></br>5.2 KSF
+        สัญญาว่าจะคุ้มครองความเป็นส่วนตัวและข้อมูลส่วนบุคคลของผู้ใช้ขงาน KSF 
+        <br></br>5.3
+        KSF
+        สัญญาที่จะใช้ความระมัดระวังและความใส่ใจอย่างสูงสุดเกี่ยวกับมาตรการรักษาความปลอดภัย
+        เพื่อให้มีการรักษาความปลอดภัยของข้อมูลใดๆ ของผู้ใช้ ทั้งหมด
+        </p>
+      </div>
+    </div>
   );
 }
