b9340b9455c265584d56f1a6adc06779096e47a8
sarinrat charoenkunasit <>
2020-11-11 02:41:50 +0700

    ✨[ADD] wait page

---
+++ src/components/Register/Wait.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Wait() {
+    return (
+        <div className="max-w-screen-xl mx-auto min-h-screen">
+            <div className="container">
+                <div className="row my-54">
+                    <div className="col">
+                    <img className="mx-auto my-5" width= '227px' src="image/wait.png"/>
+                    <p className="text-gray-700 text-center my-4">Please Wait...</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
