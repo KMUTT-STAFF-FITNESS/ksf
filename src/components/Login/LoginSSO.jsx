498b1d9de1807eb160681dc861fceff44c72b70b
sarinrat charoenkunasit <>
2020-11-19 02:28:29 +0700

    [ADD] coming soon login page

---
+++ src/components/Login/LoginSSO.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Logo from '../core/Logo'
+
+export default function LoginSSO() {
+    return (
+        <div className="max-w-screen-xl mx-auto min-h-screen">
+            <Logo/>
+            <div className="container">
+                <div className="row my-54">
+                    <div className="col">
+                    <img className="mx-auto my-5" width= '227px' src="image/wait.png"/>
+                    <p className="text-gray-700 text-center my-4">Comming soon</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
