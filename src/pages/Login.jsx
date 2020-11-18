f15f4167a667b80a1dd5eab826de4a1037f96bdc
Thinnapat <mfame2012@gmail.com>
2020-11-18 13:48:45 +0700

    update login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,6 +5,7 @@ import BtnNext from "../components/core/BtnNext";
 import BtnRigis from "../components/core/BtnBack";
 import * as Yup from "yup";
 import { navigate } from "@reach/router";
+import { Helmet } from "react-helmet";
 
 export default function Login() {
   const LoginSchema = Yup.object().shape({
@@ -17,71 +18,42 @@ export default function Login() {
   });
 
   return (
-    <div className="max-w-screen-xl mx-auto min-h-screen">
-      <div className="container">
-        <div className="row mb-5">
-          <div className="col-12 text-center">
-            <Logo />
-          </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-12">
-            <Formik
-              initialValues={{ email: "", password: "" }}
-              validationSchema={LoginSchema}
-              onSubmit={({ setSubmitting }) => {
-                alert("Form is validated! Submitting the form...");
-                setSubmitting(false);
-              }}
-            >
-              {({ touched, errors, isSubmitting }) => (
-                <Form>
-                  <div className="form-group">
-                    <Field
-                      type="email"
-                      name="email"
-                      placeholder="example@mail.kmutt.ac.th"
-                      className={`form-control ${
-                        touched.email && errors.email ? "is-invalid" : ""
-                      }`}
-                    />
-                    <ErrorMessage
-                      component="div"
-                      name="email"
-                      className="invalid-feedback"
-                    />
-                  </div>
+    // <div className="max-w-screen-xl mx-auto min-h-screen">
+    //   <div className="lg:w-2/3 bg-gray-900"></div>
+    //   <div className="w-full lg:w-1/3">
+    //     <div className="col-12 text-center">
+    //       <Logo />
+    //     </div>
+    //     <div className="row justify-content-center">
+    //       <div className="w-full lg:w-1/3 overflow-x-hidden py-4 mx-auto">
+    //         <div className=" py-4">
+    //           <BtnNext text="Login via SSO" />
+    //           <BtnRigis
+    //             text="Register via SSO"
+    //             onClick={() => navigate("/register")}
+    //           />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="flex flex-col h-screen">
+      <Helmet>
+        <title>KSF</title>
+      </Helmet>
 
-                  <div className="form-group">
-                    <Field
-                      type="password"
-                      name="password"
-                      placeholder="password"
-                      className={`form-control ${
-                        touched.password && errors.password ? "is-invalid" : ""
-                      }`}
-                    />
-                    <ErrorMessage
-                      component="div"
-                      name="password"
-                      className="invalid-feedback"
-                    />
-                  </div>
-                </Form>
-              )}
-            </Formik>
-            <div className="w-full lg:w-1/3 overflow-x-hidden py-4 mx-auto">
-                <div className="row">
-                  <div className="col-6 py-4">
-                    <BtnRigis text="Register" onClick={() => navigate("/register")} />
-                  </div>
-                  <div className="col-6 py-4">
-                    <BtnNext text="Login" />
-                  </div>
-                </div>
-              </div>
+      <div className="flex flex-1 overflow-auto">
+        <div className="w-0 lg:w-2/3 bg-black">
+          <img src="/image/sample.png" alt="" />
         </div>
-      </div>
+        <div className="w-full lg:w-1/3 px-2">
+          <Logo />
+          <BtnNext text="Login via SSO" />
+          <BtnRigis
+            text="Register via SSO"
+            onClick={() => navigate("/register")}
+          />
+        </div>
       </div>
     </div>
   );
